9ec4ebb614859de11ff0750cee109505f6ae4330
Michal Rzepka <Michal.Rzepka@partner.pass-consulting.com>
2021-12-05 21:11:40 +0100

    Create PS script for running N clients at once

---
+++ run.ps1
@@ -0,0 +1,14 @@
+ param (
+    [int]$clients = 1
+ )
+
+New-Item -ItemType Directory -Force -Path .\log
+
+for ($num = 1; $num -le $clients; $num++) {
+
+    $outPath = ".\log\console" + $num + ".out"
+    $errPath = ".\log\console" + $num + ".err"
+
+    Start-Process -NoNewWindow java -ArgumentList '-jar', 'jars\WoCoClient.jar', 'localhost', '8080', '1', '2', '1' `
+    -RedirectStandardOutput $outPath -RedirectStandardError $errPath
+}
