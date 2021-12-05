3d133304de13f6e94fa5666cc121c7f40369eebf
Michal Rzepka <Michal.Rzepka@partner.pass-consulting.com>
2021-12-05 23:19:47 +0100

    Implement tag remover

--- src/main/java/org/simple/software/server/core/TagRemoverImpl.java
+++ src/main/java/org/simple/software/server/core/TagRemoverImpl.java
@@ -1,10 +1,60 @@
 package org.simple.software.server.core;
 
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TagRemoverImpl implements TagRemover {
+
+    private static final String HTML_TAG = "(<)(.*?)(>)";
+
+    private static final String TITLE_ATTR = "(title=\")(.*?)(\")";
+    private static final int TITLE_ATTR_VALUE_POS_IN_REGEXP_GROUP = 2;
 
     @Override
     public String removeTags(String document) {
-        // TODO provide expected impl
-        return document;
+
+        Pattern tagPattern = Pattern.compile(HTML_TAG);
+        Matcher tagMatcher = tagPattern.matcher(document);
+
+        String match;
+        String replacement;
+        int lastMatchIndex = 0;
+
+        // start with empty string
+        StringBuilder output = new StringBuilder();
+
+        // for each match:
+        // 1. add substring before match to output
+        // 2. convert match into desired form and add to output
+        // 3. repeat for all matches
+        while (tagMatcher.find()) {
+            match = tagMatcher.group();
+            replacement = extractTitle(match).orElse("");
+
+            output.append(document, lastMatchIndex, tagMatcher.start())
+                    .append(replacement);
+
+            lastMatchIndex = tagMatcher.end();
+        }
+
+        // add remaining part of document to output
+        if (lastMatchIndex < document.length()) {
+            output.append(document, lastMatchIndex, document.length());
+        }
+
+        return output.toString();
+
+    }
+
+    private Optional<String> extractTitle(String str) {
+        Pattern titlePattern = Pattern.compile(TITLE_ATTR);
+        Matcher titleMatcher = titlePattern.matcher(str);
+
+        if (titleMatcher.find()) {
+            return Optional.of(titleMatcher.group(TITLE_ATTR_VALUE_POS_IN_REGEXP_GROUP) + " ");
+        }
+
+        return Optional.empty();
     }
 }
---
+++ src/test/java/org/simple/software/server/core/TagRemoverImplTest.java
@@ -0,0 +1,49 @@
+package org.simple.software.server.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TagRemoverImplTest {
+
+    TagRemoverImpl tagRemover = new TagRemoverImpl();
+
+    @Test
+    void removes_tags__case1() {
+        String html = "<p><b>foo</p></b>";
+
+        String result = tagRemover.removeTags(html);
+
+        assertEquals("foo", result);
+    }
+
+    @Test
+    void removes_tags__case2() {
+        String html = "<p><b>foo</p></b> <td>bar</td>";
+
+        String result = tagRemover.removeTags(html);
+
+        assertEquals("foo bar", result);
+    }
+
+    @Test
+    void spares_text_attrib__case1() {
+        String html = "<a href=\"xyz\" title=\"FooBar\">foobar</a>";
+
+        String result = tagRemover.removeTags(html);
+
+        assertEquals("FooBar foobar", result);
+    }
+
+    @Test
+    void spares_text_attrib__case2() {
+        String html = "<a href=\"xyz\" title=\"FooBar\">foobar</a> <head title=\"FizzBuzz\">buzz</head>";
+
+        String result = tagRemover.removeTags(html);
+
+        assertEquals("FooBar foobar FizzBuzz buzz", result);
+    }
+
+
+
+}
