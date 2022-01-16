7f3a74ee1cae1c1150711d7beaba954193d01b6c
Michal Rzepka <Michal.Rzepka@partner.pass-consulting.com>
2022-01-16 18:09:26 +0100

    Rename client logs path to log-clients

--- run.ps1
+++ run.ps1
@@ -2,13 +2,13 @@
     [int]$clients = 1
  )
 
-New-Item -ItemType Directory -Force -Path .\log
+New-Item -ItemType Directory -Force -Path .\log-clients
 
 for ($num = 1; $num -le $clients; $num++) {
 
     $outPath = ".\log\console" + $num + ".out"
     $errPath = ".\log\console" + $num + ".err"
 
-    Start-Process -NoNewWindow java -ArgumentList '-jar', 'jars\WoCoClient.jar', 'localhost', '8080', '20', '2', '1' `
+    Start-Process -NoNewWindow java -ArgumentList '-jar', 'jars\WoCoClient.jar', 'localhost', '25000', '1', '1', '1' `
     -RedirectStandardOutput $outPath -RedirectStandardError $errPath
 }
