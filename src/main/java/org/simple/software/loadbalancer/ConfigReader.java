22f33f85a664aa2080d8ee2db1e8276b32873a25
Michal Rzepka <Michal.Rzepka@partner.pass-consulting.com>
2022-01-16 18:06:36 +0100

    Reading config file: Implement simple comments

--- src/main/java/org/simple/software/loadbalancer/ConfigReader.java
+++ src/main/java/org/simple/software/loadbalancer/ConfigReader.java
@@ -3,6 +3,7 @@ package org.simple.software.loadbalancer;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -13,14 +14,20 @@ class ConfigReader {
 
         return reader.lines()
                 .map(ConfigReader::lineToAddress)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
                 .collect(Collectors.toSet());
     }
 
-    private static ServerAddress lineToAddress(String line) {
+    private static Optional<ServerAddress> lineToAddress(String line) {
+        if (line.startsWith("#")) {
+            return Optional.empty();
+        }
+
         String[] splitted = line.split(":");
         String host = splitted[0];
         int port = Integer.parseInt(splitted[1]);
 
-        return new ServerAddress(host, port);
+        return Optional.of(new ServerAddress(host, port));
     }
 }
