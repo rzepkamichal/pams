646bd3429bb8e8bf9f16f5c2e55514b6ab08c251
Michal Rzepka <Michal.Rzepka@partner.pass-consulting.com>
2022-01-31 11:32:43 +0100

    Adjust params in queue models

--- queue_models.R
+++ queue_models.R
@@ -16,16 +16,16 @@ mmmQ_ert <- function(m, mu, lambda) {
   
   pq = (((m * ro)^m) / (factorial(m) * (1 - ro))) * p0
   
-  er = 1 / mu * (1 + pq / (m * (1 - ro))) * 1000 - 0.5
+  er = 1 / mu * (1 + pq / (m * (1 - ro))) * 1000 - 0.3
 }
 
 loadedRecords <- read.csv(file = 'data.csv')
 
 # number of processing units
-m = 4
+m = 3.5
 
 # service rate
-mu = 595
+mu = 660
 
 results <- lapply(loadedRecords, mmmQ_ert, m = m, mu = mu)
 write.csv(results, "output.csv", row.names = FALSE)
@@ -42,17 +42,17 @@ mva <- function() {
   P <- list(c(1), c(1), c(1))
   
   # fixed capacity: mean service time
-  S <- c(0.00016, 0, 0)
+  S <- c(0.00017, 0, 0)
   
   mu = 2041
   # service rate: load dependent server
-  u <- list(c(0), c(mu, 1.85 * mu), c(mu, 1.85 * mu))
+  u <- list(c(0), c(mu, 1.75 * mu), c(mu, 1.75 * mu))
   
   # num of visits to i-th device
   Vi <- c(1, 0.5, 0.5)
   
   # thinking time
-  Z = 0.00098
+  Z = 0.0008
   
   # system response time
   R = 0
@@ -181,7 +181,7 @@ mva <- function() {
   
     if ((n != 15 && n <= 16) || n == 18 || n == 24 || n == 32 || n == 64) {
       
-      tputs = append(tputs, X)
+      tputs = append(tputs, R * 1000)
       utilizations <- rbind(utilizations, data.frame(Ui[1], Ui[2], Ui[3]))
     }
     
@@ -194,6 +194,6 @@ mva <- function() {
   
 }
 
-#print (mva())
+print (mva())
 
-#write.csv(mva(), "output.csv", row.names = FALSE)
+write.csv(mva(), "output.csv", row.names = FALSE)
